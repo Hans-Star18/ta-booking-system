cd36ea86f5d60fac6228d0303956a4af2ec8ee10
Agus Suardiasa <agussuardiasa1231@gmail.com>
2025-03-01 08:27:33 +0800

    feat: initial commit and connect docker configuration

---
+++ resources/js/app.jsx
@@ -0,0 +1,24 @@
+import "./bootstrap";
+import "../css/app.css";
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+import { route as routeFn } from "ziggy-js";
+
+global.route = routeFn;
+
+const appName = import.meta.env.VITE_APP_NAME;
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => {
+        const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
+        return pages[`./pages/${name}.jsx`];
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+    progress: {
+        color: "#fff",
+        showSpinner: true,
+    },
+});
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/js/app.jsx"],
+            refresh: true,
+        }),
+        tailwindcss(),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+        },
+    },
+    server: {
+        host: "0.0.0.0", // Dengarkan di semua antarmuka jaringan
+        port: 5173, // Port default Vite
+        hmr: {
+            host: "localhost", // Host untuk Hot Module Replacement (HMR)
+        },
+    },
+});
