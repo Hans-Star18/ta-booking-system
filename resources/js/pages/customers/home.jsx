c8cef8287e215c98e6ce796d20511cd3c381dd2c
Hans-Star18 <agussuardiasa1231@gmail.com>
2025-03-22 15:54:48 +0800

    add datepicker into selected check avaibility

--- resources/js/pages/customers/home.jsx
+++ resources/js/pages/customers/home.jsx
@@ -7,6 +7,12 @@ import Input from "@/components/form/input";
 export default function Home() {
     const [currentStep, setCurrentStep] = useState(1);
 
+    const [dateOfBirth, setDateOfBirth] = useState("");
+
+    const handleDateChange = (date) => {
+        setDateOfBirth(date.toLocaleDateString()); // Handle selected date and format it
+    };
+
     return (
         <>
             <Head title="Home"></Head>
@@ -16,7 +22,7 @@ export default function Home() {
 
                 <div className="w-full bg-blue-300 my-6 p-6 rounded-md">
                     <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-10">
-                        <div>
+                        <div className="relative">
                             <Input
                                 id={"check-in"}
                                 name={"check-in"}
@@ -24,8 +30,25 @@ export default function Home() {
                                 type="date"
                                 className="bg-white"
                             />
+
+                            <span className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
+                                    />
+                                </svg>
+                            </span>
                         </div>
-                        <div>
+                        <div className="relative">
                             <Input
                                 id={"check-out"}
                                 name={"check-out"}
@@ -33,6 +56,23 @@ export default function Home() {
                                 type="date"
                                 className="bg-white"
                             />
+
+                            <span className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
+                                    />
+                                </svg>
+                            </span>
                         </div>
                         <div className="col-span-2 md:col-span-1">
                             <button className="w-full bg-blue-500 text-white shadow-xs hover:bg-blue-600 inline-flex items-center justify-center gap-2 rounded-lg transition px-4 py-3 text-sm">
