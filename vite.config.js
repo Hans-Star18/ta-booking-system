aa784217b2688b820f4d70e0bd928b7b702e5444
Agus Suardiasa <agussuardiasa1231@gmail.com>
2025-07-30 03:10:13 +0800

    update vite prod

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,25 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import Path from "path";
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import Path from 'path'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.jsx"],
+            input: ['resources/js/app.jsx'],
             refresh: true,
         }),
         tailwindcss(),
         react(),
     ],
     esbuild: {
-        jsx: "automatic",
+        jsx: 'automatic',
     },
     resolve: {
         alias: {
-            "@": Path.resolve(__dirname, "resources/js"),
-            "ziggy-js": Path.resolve(__dirname, "vendor/tightenco/ziggy"),
+            '@': Path.resolve(__dirname, 'resources/js'),
+            'ziggy-js': Path.resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
-    server: {
-        host: "0.0.0.0", // Dengarkan di semua antarmuka jaringan
-        port: 5174, // Port default Vite
-        hmr: {
-            host: "localhost", // Host untuk Hot Module Replacement (HMR)
-        },
-    },
-});
+})
