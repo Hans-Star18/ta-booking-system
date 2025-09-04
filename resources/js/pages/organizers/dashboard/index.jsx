cabd8a7a3e208f73625f53204f3a2202bdc54848
Agus Suardiasa <agussuardiasa1231@gmail.com>
2025-09-04 23:21:33 +0800

    feat(organizer-dashboard): add basic chart to organizer dashboard

--- resources/js/pages/organizers/dashboard/index.jsx
+++ resources/js/pages/organizers/dashboard/index.jsx
@@ -1,7 +1,58 @@
 import OrganizerLayout from '@/layouts/organizer-layout'
 import { Head } from '@inertiajs/react'
+import { useState } from 'react'
+import ReactApexChart from 'react-apexcharts'
 
 export default function Index() {
+    const [chartData, _] = useState({
+        series: [
+            {
+                name: 'Actual',
+                data: [
+                    {
+                        x: '2011',
+                        y: 1292,
+                    },
+                    {
+                        x: '2012',
+                        y: 4432,
+                    },
+                    {
+                        x: '2013',
+                        y: 5423,
+                    },
+                    {
+                        x: '2014',
+                        y: 6653,
+                    },
+                    {
+                        x: '2015',
+                        y: 8133,
+                    },
+                    {
+                        x: '2016',
+                        y: 7132,
+                    },
+                    {
+                        x: '2017',
+                        y: 7332,
+                    },
+                    {
+                        x: '2018',
+                        y: 6553,
+                    },
+                ],
+            },
+        ],
+        options: {
+            chart: {
+                height: 350,
+                type: 'bar',
+            },
+            colors: ['#2b7fff'],
+        },
+    })
+
     return (
         <>
             <Head title="Organizer Dashboard" />
@@ -12,7 +63,14 @@ export default function Index() {
                         <h1 className="text-2xl font-bold">Dashboard</h1>
                     </div>
 
-                    <div className="max-w-[1200px] overflow-x-auto"></div>
+                    <div className="max-w-[1200px] overflow-x-auto">
+                        <ReactApexChart
+                            options={chartData.options}
+                            series={chartData.series}
+                            type="bar"
+                            height={350}
+                        />
+                    </div>
                 </div>
             </OrganizerLayout>
         </>
